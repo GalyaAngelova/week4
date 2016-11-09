1c3c3f2129e6c258c650a67db4d4918bdedfe941
GalyaAngelova <g.angelovaa@abv.bg>
2016-11-09 21:27:40 +0200

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main () {
 int num;
     cout << "Enter n: " ;
-    cin >> n;
+    cin >> num;
     int n100,n10,n1;
-    n100 = n / 100;
-    n10 = n / 10%10;
-    n1 = n % 10;
+    n100 = num / 100;
+    n10 = num / 10%10;
+    n1 = num % 10;
     if (n100*n10*n1 >= 100){
         cout << "YES," << n100*n10*n1 - n100 - n10 - n1 << "." << endl;
     }
-    else cout << "NO," << n100*n10*n << "." << endl;
+    else cout << "NO," << n100*n10*n1 << "." << endl;
     system("pause");
     return 0;
 }
